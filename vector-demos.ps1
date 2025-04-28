872585868f819a01591912fc28df560f8db41e91
Anthony E. Nocentino <nocentino@users.noreply.github.com>
2025-04-28 11:06:16 -0500

    updates

--- vector-demos.ps1
+++ vector-demos.ps1
@@ -10,7 +10,7 @@ $Credential = New-Object System.Management.Automation.PSCredential($Username, $P
 
 # Define the SQL Server instance and database
 $Database = "AdventureWorks2025"
-$SqlInstance = Connect-DbaInstance -SqlInstance 'localhost' -SqlCredential $Credential -Database $Database -TrustServerCertificate
+$SqlInstance = Connect-DbaInstance -SqlInstance 'localhost' -SqlCredential $Credential -TrustServerCertificate
 
 
 # Example usage with Invoke-DbaQuery
@@ -23,3 +23,17 @@ Invoke-DbaQuery -SqlInstance $SqlInstance  -Query $query
 
 
 
+# Example usage with Invoke-DbaQuery
+# Define the query to execute
+$query = @"
+--CREATE DATABASE [TestDB];
+GO
+--CREATE TABLE [TestDB].[dbo].[TestTable1] (ID INT, Name VECTOR(3));
+INSERT INTO [TestDB].[dbo].[TestTable1] VALUES (1, '[1,2,3]');
+"@
+Invoke-DbaQuery -SqlInstance $SqlInstance  -Query $query 
+
+$query = @"
+SELECT * FROM [TestDB].[dbo].[TestTable1];
+"@
+Invoke-DbaQuery -SqlInstance $SqlInstance  -Query $query -Database "TestDB"
