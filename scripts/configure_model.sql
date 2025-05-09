2174f1ddd040173fdc28a25cd574718236c538dd
Anthony E. Nocentino <aen@centinosystems.com>
2025-05-09 14:41:38 -0500

    fixes

--- scripts/configure_model.sql
+++ scripts/configure_model.sql
@@ -14,8 +14,8 @@ PRINT 'Creating External model "ollama"...';
 GO
 CREATE EXTERNAL MODEL ollama
 WITH (
-    LOCATION = 'https://model-web:443/api/embeddings',
-    MODEL_PROVIDER = 'Ollama',
+    LOCATION = 'https://model-web:443/api/embed',
+    API_FORMAT = 'Ollama',
     MODEL_TYPE = EMBEDDINGS,
     MODEL = 'nomic-embed-text'
 );
@@ -25,7 +25,8 @@ PRINT 'Testing the external model by calling get_embeddings function...';
 GO
 BEGIN
     DECLARE @result NVARCHAR(MAX);
-    SET @result = (SELECT CONVERT(NVARCHAR(MAX), get_embeddings(ollama, N'test text')));
+    SET @result = (SELECT CONVERT(NVARCHAR(MAX), AI_GENERATE_EMBEDDINGS(N'test text' MODEL ollama)))
+    SELECT AI_GENERATE_EMBEDDINGS(N'test text' MODEL ollama) 
 
     IF @result IS NOT NULL
         PRINT 'Model test successful. Result: ' + @result;
