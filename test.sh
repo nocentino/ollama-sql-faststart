b4302ca4b124822767c21f32395d71cc419a14a4
Anthony Nocentino <aen@centinosystems.com>
2025-05-12 20:21:40 +0000

    comments

--- test.sh
+++ test.sh
@@ -1,7 +1,12 @@
+# Start up the Ollama SQL Faststart demo with Docker Compose
+# This script will start up the Ollama SQL Faststart demo with Docker Compose
 docker compose up --detach 
 
 # Head over to vector-demos.sql to configure the database and run the demos
 
+
+# Clean up the Docker containers and volumes after the demo
+# This script will stop and remove the Docker containers, volumes and clean up the certificates
 docker compose down && \
 docker volume rm ollama-sql-faststart_sql-data && \
 docker volume rm ollama-sql-faststart_ollama_models &&
