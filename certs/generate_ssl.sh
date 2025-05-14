9235270319a9869f048fbedbfdab37b74aa40cc0
Anthony E. Nocentino <aen@centinosystems.com>
2025-05-14 07:59:23 -0500

    cert

--- certs/generate_ssl.sh
+++ certs/generate_ssl.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-if [ ! -f /certs/nginx.key ] || [ ! -f /certs/nginx.crt ]; then
-  echo "Generating SSL certificates..."
-  openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
-    -keyout /certs/nginx.key -out /certs/nginx.crt \
-    -config /tmp/certs/openssl.cnf
-  echo "Generated SSL certificates..."
-else
-  echo "SSL certificates already exist. Skipping generation."
-fi
+echo "Generating SSL certificates..."
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
+  -keyout /certs/nginx.key -out /certs/nginx.crt \
+  -config /tmp/certs/openssl.cnf
+echo "Generated SSL certificates..."
